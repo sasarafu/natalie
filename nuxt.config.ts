14a6867405ad1c40f472055918f5286b9309bf39
sasarafu <138344965+sasarafu@users.noreply.github.com>
2023-07-08 18:29:10 +0900

    add vueuse and pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt', '@pinia/nuxt'],
+  components: [{ path: '~/components/instances', prefix: '' }, '~/components'],
+  pinia: {
+    autoImports: ['defineStore'],
+  },
 });
