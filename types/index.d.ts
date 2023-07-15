75eac7ab418ba10b575e453df1607f7acdbbc6fa
sasarafu <138344965+sasarafu@users.noreply.github.com>
2023-07-15 14:51:06 +0900

    lintfix

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,15 +1,19 @@
-import { IRepositories } from "~/factories/repositoryFactory";
+import { IRepositories } from '~/factories/repositoryFactory';
 
 declare module '#app' {
   interface NuxtApp {
-    $repositories: <key extends keyof IRepositories>(name: key) => ReturnType<IRepositories[key]>;
+    $repositories: <key extends keyof IRepositories>(
+      name: key,
+    ) => ReturnType<IRepositories[key]>;
   }
 }
 
 declare module 'vue' {
   interface ComponentCustomPoperties {
-    $repositories: <key extends keyof IRepositories>(name: key) => ReturnType<IRepositories[key]>;
+    $repositories: <key extends keyof IRepositories>(
+      name: key,
+    ) => ReturnType<IRepositories[key]>;
   }
 }
 
-export { }
+export {};
