538bf40a9008dfe468df18a68959e63c5388c029
sasarafu <138344965+sasarafu@users.noreply.github.com>
2023-07-17 14:17:15 +0900

    redirect to login page when no loginusers

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="flex gap-x-1 h-screen w-screen bg-base-300">
-    <CommonSidebar />
+  <div>
+    <template v-if="loginUsers.length">
+      <div class="flex gap-x-1 h-screen w-screen bg-base-300">
+        <CommonSidebar />
+      </div>
+    </template>
   </div>
 </template>
+
+<script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import { useLoginUsers } from '~/stores/loginUsers';
+
+const { loginUsers } = storeToRefs(useLoginUsers());
+
+if (loginUsers.value.length === 0) {
+  navigateTo('/login');
+}
+</script>
