49bfa323fc4c90f3a0117a64980754a110f32f3c
sasarafu <138344965+sasarafu@users.noreply.github.com>
2023-07-22 11:44:44 +0900

    show timelines

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,14 @@
     <template v-if="loginUsers.length">
       <div class="flex gap-x-1 h-screen w-screen bg-base-300">
         <CommonSidebar />
+        <div class="flex-auto flex gap-x-1 overflow-x-auto">
+          <CommonColumnTimeline
+            v-for="timeline in timelines"
+            :key="timeline.id"
+            :column-name="timeline.name"
+            :items="datasources[timeline.id]"
+          />
+        </div>
       </div>
     </template>
   </div>
@@ -10,11 +18,20 @@
 
 <script setup lang="ts">
 import { storeToRefs } from 'pinia';
+import { useDatasources } from '~/stores/datasources';
 import { useLoginUsers } from '~/stores/loginUsers';
+import { useTimelines } from '~/stores/timelines';
 
+const { datasources } = storeToRefs(useDatasources());
 const { loginUsers } = storeToRefs(useLoginUsers());
+const { timelines } = storeToRefs(useTimelines());
 
 if (loginUsers.value.length === 0) {
   navigateTo('/login');
 }
+
+// initialize timelines
+timelines.value.forEach((timeline) => {
+  datasources.value[timeline.id] = [];
+});
 </script>
