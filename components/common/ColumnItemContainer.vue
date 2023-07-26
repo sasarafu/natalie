1b13251e7b05732d2d1e07d856056107b9d2e6d8
sasarafu <138344965+sasarafu@users.noreply.github.com>
2023-07-26 23:46:38 +0900

    fix columnitem padding

--- components/common/ColumnItemContainer.vue
+++ components/common/ColumnItemContainer.vue
@@ -1,5 +1,5 @@
 <template>
-  <article class="flex gap-x-2 p-2">
+  <article class="flex gap-x-2 p-2 pt-3">
     <div class="flex-none">
       <a href="#" tabindex="-1">
         <CommonPartsRoundedIcon :icon-url="iconUrl" class="w-9 h-9" />
