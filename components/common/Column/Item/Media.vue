bdf76342628a6167ffb5377d231f0bb55c9fec47
sasarafu <138344965+sasarafu@users.noreply.github.com>
2023-08-11 15:28:35 +0900

    add medialist in timeline

---
+++ components/common/Column/Item/Media.vue
@@ -0,0 +1,76 @@
+<template>
+  <div
+    v-if="isVisible"
+    class="relative cursor-zoom-in overflow-hidden"
+    @click="$emit('click')"
+  >
+    <video
+      v-if="media.type === 'video'"
+      class="w-full h-full object-cover rounded"
+      :poster="media.thumbnailUrl"
+      preload="none"
+      controls
+      controlslist="nodownload"
+      muted
+    >
+      <source :src="media.url" :type="media.detailedType" />
+    </video>
+
+    <img
+      v-if="media.type === 'image'"
+      :src="media.thumbnailUrl"
+      class="w-full h-full object-cover rounded"
+      draggable="false"
+    />
+
+    <div
+      v-if="media.type === 'audio'"
+      class="w-full h-full flex items-center rounded bg-neutral-focus"
+    >
+      <audio
+        :src="media.thumbnailUrl"
+        class="w-full"
+        controls
+        controlslist="nodownload"
+      />
+    </div>
+
+    <button
+      v-if="media.sensitive"
+      type="button"
+      class="btn btn-xs btn-square btn-neutral absolute right-1 top-1"
+      @click.stop="isVisible = false"
+    >
+      <span class="material-symbols-outlined text-base">visibility_off</span>
+    </button>
+  </div>
+
+  <button
+    v-else
+    type="button"
+    class="w-full h-full rounded bg-neutral"
+    @click="isVisible = true"
+  >
+    <div class="flex justify-center items-center">
+      <span class="">sensitive</span>
+    </div>
+  </button>
+</template>
+
+<script setup lang="ts">
+import type { IMedia } from '~/models/common/media';
+
+const props = defineProps<{
+  media: IMedia;
+}>();
+
+defineEmits<{
+  (e: 'click'): void;
+}>();
+
+const isVisible = ref(true);
+
+onMounted(() => {
+  isVisible.value = !props.media.sensitive;
+});
+</script>
---
+++ components/common/Column/Item/MediaList.vue
@@ -0,0 +1,33 @@
+<template>
+  <div v-if="mediaList.length" class="relative">
+    <div class="grid gap-1 aspect-video" :class="gridClass">
+      <template v-for="media in mediaList.slice(0, 4)" :key="media">
+        <CommonColumnItemMedia
+          :media="media"
+          :class="{ 'first:row-span-2': mediaList.length === 3 }"
+        />
+      </template>
+    </div>
+    <span
+      v-if="mediaList.length > 4"
+      class="badge badge-primary absolute -bottom-2.5 right-0"
+    >
+      +{{ mediaList.length - 4 }}
+    </span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { IMedia } from '~/models/common/media';
+
+const props = defineProps<{
+  mediaList: IMedia[];
+}>();
+
+const gridClass =
+  props.mediaList.length === 1
+    ? 'grid-cols-1 grid-rows-1'
+    : props.mediaList.length === 2
+    ? 'grid-cols-2 grid-rows-1'
+    : 'grid-cols-2 grid-rows-2';
+</script>
