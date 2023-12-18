7264a685badaebe1ae21e6de7471a503d67f8405
sasarafu <138344965+sasarafu@users.noreply.github.com>
2023-12-19 01:18:59 +0900

    restructure components directory

--- pages/index.vue
+++ pages/index.vue
@@ -4,12 +4,12 @@
       <div class="flex gap-x-1 h-screen w-screen bg-base-300">
         <CommonSidebar />
         <div class="flex-auto flex gap-x-1 overflow-x-auto">
-          <CommonColumnTimeline
+          <CommonTimeline
             v-for="timeline in timelines"
             :key="timeline.id"
             :timeline="timeline"
           />
-          <CommonColumnMenu class="ml-auto" />
+          <CommonMenu class="ml-auto" />
         </div>
       </div>
 
