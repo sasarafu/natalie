724026d6e52c608667adef4942277520d3b2b2e2
sasarafu <138344965+sasarafu@users.noreply.github.com>
2024-02-07 23:30:25 +0900

    add repost favorite

--- components/instances/bluesky/ColumnItem.vue
+++ components/instances/bluesky/ColumnItem.vue
@@ -24,6 +24,48 @@
     </p>
 
     <CommonTimelineItemMedia :media-list="mediaList" />
+
+    <template #footer>
+      <div class="flex gap-x-1 mt-1">
+        <button type="button" class="btn btn-xs btn-ghost" tabindex="-1">
+          <span class="material-symbols-outlined text-base">reply</span>
+        </button>
+        <button
+          type="button"
+          class="btn btn-xs btn-ghost"
+          :class="{ 'text-yellow-500': cachedReposted }"
+          tabindex="-1"
+          @click="toggleRepost"
+        >
+          <span class="material-symbols-outlined text-base">
+            {{ cachedReposted ? 'published_with_changes' : 'sync' }}
+          </span>
+          <span v-if="actualItem.body.post.repostCount">
+            {{ actualItem.body.post.repostCount }}
+          </span>
+        </button>
+        <button
+          type="button"
+          class="btn btn-xs btn-ghost"
+          :class="{ 'text-yellow-500': cachedLiked }"
+          tabindex="-1"
+          @click="toggleLike"
+        >
+          <span
+            class="material-symbols-outlined text-base"
+            :class="{ filled: cachedLiked }"
+          >
+            favorite
+          </span>
+          <span v-if="actualItem.body.post.likeCount">
+            {{ actualItem.body.post.likeCount }}
+          </span>
+        </button>
+        <button type="button" class="btn btn-xs btn-ghost" tabindex="-1">
+          <span class="material-symbols-outlined text-base">more_horiz</span>
+        </button>
+      </div>
+    </template>
   </CommonTimelineItemContainer>
 </template>
 
@@ -36,6 +78,50 @@ const props = defineProps<{
 }>();
 
 const actualItem = computed(() => props.item);
+
+const cachedReposted = ref(props.item.body.post.viewer?.repost);
+const cachedLiked = ref(props.item.body.post.viewer?.like);
+
+const toggleRepost = async () => {
+  try {
+    if (cachedReposted.value) {
+      await useApiClientsStore()
+        .get<'bluesky'>(props.item.via)
+        .deleteRepost(cachedReposted.value);
+      cachedReposted.value = undefined;
+    } else {
+      const ret = await useApiClientsStore()
+        .get<'bluesky'>(props.item.via)
+        .repost(props.item.body.post.uri, props.item.body.post.cid);
+      cachedReposted.value = ret.uri;
+    }
+  } catch {
+    useToastsStore().add({
+      text: 'failed to set repost',
+      level: 'error',
+    });
+  }
+};
+const toggleLike = async () => {
+  try {
+    if (cachedLiked.value) {
+      await useApiClientsStore()
+        .get<'bluesky'>(props.item.via)
+        .deleteLike(cachedLiked.value);
+      cachedLiked.value = undefined;
+    } else {
+      const ret = await useApiClientsStore()
+        .get<'bluesky'>(props.item.via)
+        .like(props.item.body.post.uri, props.item.body.post.cid);
+      cachedLiked.value = ret.uri;
+    }
+  } catch {
+    useToastsStore().add({
+      text: 'failed to set like',
+      level: 'error',
+    });
+  }
+};
 
 const mediaList = computed<IMedia[]>(() => {
   const images = actualItem.value.body.post.embed?.images;
