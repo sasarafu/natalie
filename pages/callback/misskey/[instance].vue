e4d15334f0f78fb442f939a99a16e2e91f826ddd
sasarafu <138344965+sasarafu@users.noreply.github.com>
2024-02-10 23:18:23 +0900

    fix misskey callback page

--- pages/callback/misskey/[instance].vue
+++ pages/callback/misskey/[instance].vue
@@ -1,36 +1,47 @@
 <template>
   <div>
     リダイレクトされないようならボタンを押してください
-    <button type="button" class="btn btn-primary">
+    <button type="button" class="btn">
       <NuxtLink to="/">back to application</NuxtLink>
     </button>
   </div>
 </template>
 
 <script setup lang="ts">
-const { $repositories } = useNuxtApp();
+import type { entities } from 'misskey-js';
+
 const $route = useRoute();
-const { add } = useLoginUsersStore();
 
 const instanceUrl = $route.params.instance;
 const sessionId = $route.query.session;
 
 if (typeof instanceUrl === 'string' && typeof sessionId === 'string') {
-  const res = await $repositories('misskey').getAccessToken(
-    instanceUrl,
-    sessionId,
-  );
+  try {
+    const res = await $fetch<{
+      ok: boolean;
+      token: string;
+      user: entities.User;
+    }>(`https://${instanceUrl}/api/miauth/${sessionId}/check`, {
+      method: 'post',
+    });
 
-  if (res?.ok) {
-    add({
+    if (!res.ok) {
+      throw new Error('not ok');
+    }
+
+    useLoginUsersStore().add({
       instance: {
         type: 'misskey',
         baseUrl: `https://${instanceUrl}`,
       },
       accessToken: res.token,
     });
-
-    navigateTo('/');
+  } catch {
+    useToastsStore().add({
+      text: 'ログインできませんでした',
+      level: 'error',
+    });
   }
+  navigateTo('/');
 }
 </script>
