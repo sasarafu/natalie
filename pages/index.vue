d12f1cd4f051d6b10aa68cb56c40d9e6bb92b907
sasarafu <138344965+sasarafu@users.noreply.github.com>
2024-02-16 22:45:02 +0900

    add timeline snap-x centering

--- pages/index.vue
+++ pages/index.vue
@@ -3,13 +3,16 @@
     <template v-if="orderedLoginUsers?.length">
       <div class="flex gap-1 h-screen w-screen bg-base-300 max-sm:flex-col">
         <CommonSidebar class="max-sm:order-last" />
-        <div class="flex-auto flex gap-x-1 overflow-x-auto">
+        <div
+          class="flex-auto flex gap-x-1 overflow-x-auto overscroll-none max-sm:snap-x max-sm:snap-mandatory"
+        >
           <CommonTimeline
             v-for="timeline in timelines"
             :key="timeline.id"
             :timeline="timeline"
+            class="max-sm:snap-center"
           />
-          <CommonMenu class="ml-auto" />
+          <CommonMenu class="ml-auto max-sm:snap-end" />
         </div>
       </div>
     </template>
