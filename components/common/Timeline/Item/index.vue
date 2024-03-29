42f6d8a890f4280a03c0f6080dc801fa361b7c61
sasarafu <138344965+sasarafu@users.noreply.github.com>
2024-03-30 01:29:06 +0900

    divide TimelineItem

---
+++ components/common/Timeline/Item/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <component
+    :is="columnItemComponents[item.via.instance.type]"
+    :item="item"
+    :is-last="isLast"
+  />
+</template>
+
+<script setup lang="ts">
+import {
+  BlueskyColumnItem,
+  MastodonColumnItem,
+  MisskeyColumnItem,
+} from '#components';
+import type { IMessage } from '~/models/common/message';
+
+defineProps<{
+  item: IMessage;
+  isLast: boolean;
+}>();
+
+const columnItemComponents = {
+  bluesky: BlueskyColumnItem,
+  mastodon: MastodonColumnItem,
+  misskey: MisskeyColumnItem,
+};
+</script>
