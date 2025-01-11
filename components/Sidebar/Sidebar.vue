07b2c559b66b14794c1fbfb420ae95b67663ca54
sasarafu <138344965+sasarafu@users.noreply.github.com>
2025-01-11 23:47:24 +0900

    impl basic layout

---
+++ components/Sidebar/Sidebar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { Cog6ToothIcon, PencilSquareIcon } from '@heroicons/vue/20/solid';
+</script>
+
+<template>
+  <header class="flex flex-col bg-base-200 p-2">
+    <div>
+      <button type="button" class="btn btn-sm btn-circle p-1 btn-primary">
+        <PencilSquareIcon class="size-full" />
+      </button>
+    </div>
+    <div class="grow" />
+    <div>
+      <button type="button" class="btn btn-sm btn-circle p-1 btn-ghost">
+        <Cog6ToothIcon class="size-full" />
+      </button>
+    </div>
+  </header>
+</template>
