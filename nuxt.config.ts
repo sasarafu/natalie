0f34f67d96587db8f6400a8d8d8999a4c4708a53
sasarafu <138344965+sasarafu@users.noreply.github.com>
2025-01-11 23:42:41 +0900

    install tailwind and daisyui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss'],
   ssr: false,
 });
