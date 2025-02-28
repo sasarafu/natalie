489777ac8aa91f39d250b49bd476e3adf07c50b5
sasarafu <138344965+sasarafu@users.noreply.github.com>
2025-02-28 23:17:06 +0900

    fix loginuser model

--- models/user.ts
+++ models/user.ts
@@ -1,4 +1,5 @@
 import type { UUID } from 'node:crypto';
+import type { Instance } from './instance';
 import type { BskyUser } from './instances/bsky';
 import type { MastodonUser } from './instances/mastodon';
 import type { MisskeyUser } from './instances/misskey';
@@ -6,8 +7,11 @@ import type { MisskeyUser } from './instances/misskey';
 export type InstanceUserInfo = BskyUser | MastodonUser | MisskeyUser;
 
 export type LoginUserInfo = {
+  type: Instance;
+  baseUrl: string;
   id: UUID;
   accessToken: string;
 };
 
-export type User = InstanceUserInfo & LoginUserInfo;
+export type User<Instance extends InstanceUserInfo = InstanceUserInfo> =
+  Instance & LoginUserInfo;
