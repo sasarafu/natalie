b79fd5f3fca936cd404b3c2bd9139b7bb5cb5b60
sasarafu <138344965+sasarafu@users.noreply.github.com>
2025-04-21 23:24:33 +0900

    impl PartsModalBase

---
+++ components/parts/ModalBase.vue
@@ -0,0 +1,28 @@
+<template>
+  <dialog ref="dialogRef" class="modal">
+    <div class="modal-box">
+      <slot />
+    </div>
+
+    <form method="dialog" class="modal-backdrop">
+      <button>close</button>
+    </form>
+  </dialog>
+</template>
+
+<script setup lang="ts">
+const dialogRef = useTemplateRef('dialogRef');
+
+defineExpose({
+  open: () => {
+    dialogRef.value?.showModal();
+  },
+  close: () => {
+    dialogRef.value?.close();
+  },
+});
+
+defineSlots<{
+  default: unknown;
+}>();
+</script>
