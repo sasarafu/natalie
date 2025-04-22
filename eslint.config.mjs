9f2100c82b2c3e9cbacfaa63f48731d71e837deb
sasarafu <138344965+sasarafu@users.noreply.github.com>
2025-04-23 02:22:27 +0900

    fix eslint no-console

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import withNuxt from './.nuxt/eslint.config.mjs';
 
 export default withNuxt({
   rules: {
-    'no-console': 'warn',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
     'vue/html-self-closing': [
       'warn',
       {
